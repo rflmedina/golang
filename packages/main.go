879325cb6265875631395b151074a575714f2fc6
Rafaela Medina <rafaelamedinax@gmail.com>
2023-01-18 01:21:14 -0300

    added an external library

--- packages/main.go
+++ packages/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"modulo/helpers"
+
+	"github.com/badoux/checkmail"
 )
 
 // Write:  registra uma mensagem na tela.
 func main() {
 	helpers.Write()
+
+	validateThisEmail := checkmail.ValidateFormat("myemail")
+	fmt.Println(validateThisEmail)
 }
